7683e0828b2073ec3a37785922bccf5714cce7b4
vladimir Boldakov <boldakovvova06@gmail.com>
2024-10-15 13:49:01 +0300

    Piramide

--- main.cpp
+++ main.cpp
@@ -5,15 +5,22 @@ using namespace std;
 void generatePoint(int n) {
     string result = "";
     int r = 1;
+    int l = n;
     while (r <= n) {
-        result += "*";
-        cout << result << endl;
+        for (int i = l - 1; i >= 0; --i) {
+            result += " ";
+        }
+        for (int i = 0; i < (2*r - 1); ++i) {
+git            result += "*";
+        }
         ++r;
+        --l;
+        cout << result << endl;
+        result = "";
     }
-    
 }
 
 int main() {
-    generatePoint(10);
+    generatePoint(20);
     return 0;
 }
